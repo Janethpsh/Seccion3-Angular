7522a8a84a66a175d59cbff9db401c0b63bcb3b4
Janeth Psh <kjpachecogomez26@gmail.com>
2025-10-03 09:23:55 -0600

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import './style.css';
+
+//import './topics/01-basics-types';
+//import './topics/02-object-interface';
+//import './topics/03-functions';
+//import './topics/04-homework-types';
+//import './topics/05-basic-destructuring';
+import './topics/06-function-destructuring';
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = `Hola Mundo!`;
+
+console.log('Hello Mundo!!');
